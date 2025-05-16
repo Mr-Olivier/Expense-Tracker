57e7495bd032e33b51094ade25eaa59086eb8dd7
Olivier <iradukundaolivier2000@gmail.com>
2025-05-16 09:40:19 +0200

    Add SignupRequest DTO for user registration

---
+++ src/main/java/com/unilak/expensetracker/expense_tracker/payloads/request/SignupRequest.java
@@ -0,0 +1,31 @@
+package com.unilak.expensetracker.expense_tracker.payloads.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+/**
+ * DTO for user signup request
+ * @author YourName
+ * @reg YourRegistrationNumber
+ */
+@Data
+public class SignupRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
+
+    private String phoneNumber;
+}
